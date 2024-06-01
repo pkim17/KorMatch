21dadc78553cd30b55dd0be6fee70225d0fc4400
Kim, Peter Soohwan <skim3401@gatech.edu>
2024-06-01 14:03:22 -0400

    Fixed calculation by adding normalization values

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
 //        Profile maleOne = new Profile("Jaewoo", 31, 175, "San Francisco", "CEO", 5000000, 0);
 //        Profile maleTwo = new Profile("Peter", 30, 173, "Atlanta", "CTO", 1000000, 0);
 //        Profile maleThree = new Profile("David", 30, 173, "New York", "Data Scientist", 2000000, 0);
-        Profile femaleOne = new Profile("Emma", 24, 169, "New York", "Student", 0, 0);
+        Profile femaleOne = new Profile("Emma", 24, 169, "New York", "Student", 1, 0);
         Profile femaleTwo = new Profile("Scarlet", 30, 160, "New York", "Entrepreneur", 150000, 0);
         Profile femaleThree = new Profile("Annie", 27, 140, "San Francisco", "Software Engineer", 300000, 0);
 
@@ -24,7 +24,7 @@ public class Main {
         femaleList.add(femaleThree);
 
         MatchScorer matchScorer = new MatchScorer();
-        matchScorer.assignPreference(30, 165, 150000);
+        matchScorer.assignPreference(24, 169, 150000);
         matchScorer.calculateIndividualPoints(femaleList);
         matchScorer.printPoints(femaleList);
 
